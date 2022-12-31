b1d3ed2ef8dfb7bb71565fb631fb5bccb600048f
moonshadow565 <moonshadow565@hotmail.com>
2022-12-31 19:12:36 +0100

    process rtmp

--- rtmp_dump.py
+++ rtmp_dump.py
@@ -4,11 +4,13 @@ from typing import IO, Literal, Mapping, Callable, Tuple
 from time import sleep
 import re
 import sys
+import miniamf.amf0
+import miniamf.amf3
 
 RE_IP = re.compile("^(\\w+): (0x[\\dA-F]+), address: ([\\d\\.]+), port: (\\d+), familiy: (\\d+)$")
 RE_DATA = re.compile("^(\\w+): (0x[\\dA-F]+), time: (\\d+), req: (0x[\\dA-F]+), got: (0x[\\dA-F]+)$")
 
-class ABuffer:
+class ProcessBuffer:
     # Internal storage for async buffer
     data: bytes
 
@@ -56,7 +58,7 @@ class ABuffer:
         result = yield from self.read_exact(n)
         return int.from_bytes(result, byteorder)
 
-def process_rtmp(abuffer: ABuffer):
+def process_rtmp(ProcessBuffer: ProcessBuffer):
     # Header dict
     ctx_headers: Mapping[int, Tuple[int, int, int]] = {}
     # Stream dict
@@ -66,12 +68,12 @@ def process_rtmp(abuffer: ABuffer):
 
     # Process the handshake
     for _ in range(1):
-        handshake = yield from abuffer.read_exact(1 + 0x600 + 0x600)
+        handshake = yield from ProcessBuffer.read_exact(1 + 0x600 + 0x600)
         assert handshake[0] == 0x03
 
     while True:
         # Get packed flag
-        flag = yield from abuffer.read_int(1, 'little')
+        flag = yield from ProcessBuffer.read_int(1, 'little')
 
         # Unpack flag bits
         channel = flag & 0x3f
@@ -80,10 +82,10 @@ def process_rtmp(abuffer: ABuffer):
         # Read extended channel
         match channel:
             case 0:
-                channel = yield from abuffer.read_int(1, 'little')
+                channel = yield from ProcessBuffer.read_int(1, 'little')
                 channel += 64
             case 1:
-                channel = yield from abuffer.read_int(2, 'little')
+                channel = yield from ProcessBuffer.read_int(2, 'little')
                 channel += 64
             case _:
                 pass
@@ -97,19 +99,19 @@ def process_rtmp(abuffer: ABuffer):
 
         # Header types 2,1,0 have a timestamp
         if header_type <= 2:
-            timestamp = yield from abuffer.read_int(3, 'big')
+            timestamp = yield from ProcessBuffer.read_int(3, 'big')
             # Header type 1, 0 have msg length and type
             if header_type <= 1:
-                msg_len = yield from abuffer.read_int(3, 'big')
-                msg_type = yield from abuffer.read_int(1, 'little')
+                msg_len = yield from ProcessBuffer.read_int(3, 'big')
+                msg_type = yield from ProcessBuffer.read_int(1, 'little')
                 # Header type 0 has a msg stream id
                 if header_type <= 0:
-                    msg_stream_id = yield from abuffer.read_int(4, 'little')
+                    msg_stream_id = yield from ProcessBuffer.read_int(4, 'little')
                 # TODO: figure if extended timestamp contributes to msg len
                 # if timestamp == 0xFF_FF_FF:
                 #     msg_len -= 4
             if timestamp == 0xFF_FF_FF:
-                timestamp = yield from abuffer.read_int(4, 'big')
+                timestamp = yield from ProcessBuffer.read_int(4, 'big')
 
         # Backup the header
         ctx_headers[channel] = (msg_len, msg_type, msg_stream_id)
@@ -118,7 +120,7 @@ def process_rtmp(abuffer: ABuffer):
         msg_buffer = ctx_streams.setdefault(channel, bytearray())    
 
         # Push data into buffer 
-        msg_chunk = yield from abuffer.read_exact(min(max(msg_len - len(msg_buffer), 0), ctx_chunk_size))
+        msg_chunk = yield from ProcessBuffer.read_exact(min(max(msg_len - len(msg_buffer), 0), ctx_chunk_size))
         msg_buffer.extend(msg_chunk)
 
         # If we dont have enough data yet return nothing
@@ -131,37 +133,27 @@ def process_rtmp(abuffer: ABuffer):
 
         # Process message
         match msg_type:
-            # Incomplete message recieved, continue
-            case -1:
+            # Ignore: incomplete, abort, ack, user ctl, win ack size, set peer bw
+            case -1 | 2 | 3 | 4 | 5 | 6:
                 pass
-            # Set chunk size
+            # Parse: set chunk size
             case 1:
                 ctx_chunk_size = int.from_bytes(msg_data[:4], 'big', False)
-                pass
-            # Ignore: abort, ack, user ctl, win ack size, set peer bw
-            case 2 | 3 | 4 | 5 | 6:
-                pass
-            # Parse amf3 data
-            case 0xf:
-                yield 'amf3_data', msg_data
-            # Parse amf3 cmd
+            # Parse: amf3/flex msg
             case 0x11:
-                yield 'amf3_cmd', msg_data
-            # Parse amf0 data
-            case 0x12:
-                yield 'amf0_data', msg_data
-            # parse amf0 cmd
+                yield list(miniamf.amf0.Decoder(msg_data[12:], strict=False))
+            # Parse: amf0 invoke
             case 0x14:
-                yield 'amf0_cmd', msg_data
-            # Unknown packet type
-            case _:
+                yield list(miniamf.amf0.Decoder(msg_data, strict=False))
+            # Unsuported: flex_stream_send, flex_shared_obj, amf0_shared_object, amf0_data, others...
+            case 0xf | 0x10 | 0x12 | 0x13 | _:
                 raise ValueError(f"Unsuported msg type: 0x{msg_type:02x}")
 
-def process_hook(abuffer: ABuffer):
+def process_hook(ProcessBuffer: ProcessBuffer):
     fd_peer = {}
     fd_sock = {}
     while True:
-        line = yield from abuffer.read_line()
+        line = yield from ProcessBuffer.read_line()
         line = line.strip()
         match line.split(':')[0]:
             case 'end' | 'module':
@@ -179,16 +171,16 @@ def process_hook(abuffer: ABuffer):
                 op, fid, time, req, got = op, int(fid, 16), int(time), int(req, 16), int(got, 16)
                 sock = fd_sock.get(fid, 'UNK')
                 peer = fd_peer.get(fid, 'UNK')
-                data = yield from abuffer.read_exact(got)
+                data = yield from ProcessBuffer.read_exact(got)
                 assert len(data) == got
-                newline = yield from abuffer.read_exact(1)
+                newline = yield from ProcessBuffer.read_exact(1)
                 assert newline == b'\n'
                 yield (op, fid, time, sock, peer, data)
             case _:
                 raise ValueError("hook error: " + line)
 
 def read_rtmp_from_hook(source: IO, delay: float):
-    prerunner = ABuffer(process_hook)
+    prerunner = ProcessBuffer(process_hook)
     runners = {}
     while True:
         chunk = source.read(0x1000)
@@ -202,11 +194,11 @@ def read_rtmp_from_hook(source: IO, delay: float):
                 if not len(data) in {1, 0x601, 0xC01} or data[0] != 0x03:
                     runners[key] = None
                 else:
-                    runners[key] = ABuffer(process_rtmp)
+                    runners[key] = ProcessBuffer(process_rtmp)
             if runner := runners[key]:
                 for result in runner(data):
                     print(f"{hex(fid)} - {sock} {'<' if op[0] == 'r' else '>'} {peer}")
-                    print(result[0], result[1].hex(' '))
+                    print(result)
 
 # File or - for stdin
 file = sys.stdin.buffer
